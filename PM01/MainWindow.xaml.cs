c831acac1094cc8b80d3db6055db8da5b6f20c69
VM <VM@DESKTOP-9VRG8IA>
2023-04-07 13:57:26 +0300

    Добавьте файлы проекта.

---
+++ PM01/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using MS.WindowsAPICodePack.Internal;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Xml.Serialization;
+
+namespace PM01
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var vm = new MainViewModel(FindName);
+
+            Loaded += (_, _) =>
+            {
+                vm.OnLoaded();
+            };
+
+            DataContext = vm;
+        }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Func<string, object?> FindName;
+
+        public MainViewModel(Func<string, object?> findFunc)
+        {
+            FindName = findFunc;
+
+            AddRuleCommand = new RelayCommand(AddRule);
+            RemoveRuleCommand = new RelayCommand(RemoveSelectedRule);
+            RemoveSelectedRuleCommand = new RelayCommand(RemoveSelectedRule);
+            SaveRulesCommand = new RelayCommand(SaveRules);
+
+            Rules = new ObservableCollection<ObservationRule>();
+
+            Rules.CollectionChanged += (sender, args) =>
+            {
+                OnPropertyChanged(nameof(HasRules));
+                if (args.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+                    SelectedRule = Rules.Last();
+            };
+
+        }
+
+        public void OnLoaded()
+        {
+            if (File.Exists(RULES_FILE_NAME))
+                LoadRules();
+
+            var listBoxElement = FindName("logBox");
+            if (listBoxElement is ListBox)
+            {
+                var listBox = (ListBox) listBoxElement;
+                if (!listBox.Items.IsEmpty)
+                    listBox.ScrollIntoView(listBox.Items[^1]);
+            }
+        }
+
+        public ObservableCollection<ObservationRule> Rules { get; }
+
+        private ObservationRule _selectedRule;
+
+        public ObservationRule SelectedRule
+        {
+            get { return _selectedRule; }
+            set
+            {
+                _selectedRule = value;
+                OnPropertyChanged(nameof(SelectedRule));
+                OnPropertyChanged(nameof(HasSelection));
+
+                var listBoxElement = FindName("logBox");
+                if (listBoxElement is ListBox)
+                {
+                    var listBox = (ListBox)listBoxElement;
+
+                    if (!listBox.Items.IsEmpty)
+                        listBox.ScrollIntoView(listBox.Items[^1]);
+
+                    _selectedRule.EventLogs.CollectionChanged += (_, _) =>
+                    {
+                        if (SelectedRule != value)
+                            return;
+
+                        var listBox = (ListBox)listBoxElement;
+                        listBox.ScrollIntoView(listBox.Items[^1]);
+                    };
+                }
+            }
+        }
+
+        public bool HasSelection { get => _selectedRule != null; }
+
+        public bool HasRules { get => Rules.Count > 0; }
+
+        public ICommand AddRuleCommand { get; }
+        public ICommand RemoveRuleCommand { get; }
+        public ICommand RemoveSelectedRuleCommand { get; }
+        public ICommand ClearLogsCommand { get; }
+        public ICommand SaveRulesCommand { get; }
+
+        private const string RULES_FILE_NAME = "rules.json";
+
+        private void LoadRules()
+        {
+            if (File.Exists(RULES_FILE_NAME))
+            {
+                JsonSerializer serializer = new();
+
+                using (StreamReader sr = new(RULES_FILE_NAME))
+                using (JsonReader reader = new JsonTextReader(sr))
+                {
+                    var rules = serializer.Deserialize<IEnumerable<ObservationRule>>(reader);
+                    if (rules != null) foreach (var rule in rules)
+                       Rules.Add(rule);
+                }
+            }
+        }
+
+        private void SaveRules()
+        {
+            JsonSerializer serializer = new();
+            serializer.Formatting = Formatting.Indented;
+
+            using (StreamWriter sw = new(RULES_FILE_NAME))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, Rules);
+            }
+        }
+
+        private void AddRule()
+        {
+            var dialog = new Microsoft.WindowsAPICodePack.Dialogs.CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            dialog.Title = "Select a folder to watch";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (dialog.ShowDialog() == Microsoft.WindowsAPICodePack.Dialogs.CommonFileDialogResult.Ok)
+            {
+                ObservationRule rule = new(dialog.FileName);
+                Rules.Add(rule);
+            }
+        }
+        private void RemoveSelectedRule()
+        {
+            if (SelectedRule != null)
+            {
+                Rules.Remove(SelectedRule);
+                SelectedRule = null;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action action;
+        private Func<bool> canExecute;
+
+        public RelayCommand(Action action, Func<bool> canExecute = null)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+
+}
---
+++ PM01/ObservationRule.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Windows.Shapes;
+
+namespace PM01
+{
+    // Класс, представляющий правило наблюдения
+    [Serializable]
+    public class ObservationRule
+    {
+        private readonly FileSystemWatcher watcher = new();
+        public string Path { get; set; } = string.Empty;
+        public bool IncludeSubdirectories { get; set; } = true;
+
+        public bool ObserveChanges { get; set; } = true;
+
+        public bool ObserveCreation { get; set; } = true;
+
+        public bool ObserveRename { get; set; } = true;
+
+        public bool ObserveDeletion { get; set; } = true;
+
+        public bool IsEnabled { get { return watcher.EnableRaisingEvents; } set { watcher.EnableRaisingEvents = value; } }
+
+        public ObservableCollection<LogEntry> EventLogs { get; private set; } = new ObservableCollection<LogEntry>();
+
+        public ObservationRule(string path)
+        {
+            Path = path;
+
+            //Назначение обработчиков событий
+            watcher.Created += OnCreated;
+            watcher.Deleted += OnDeleted;
+            watcher.Renamed += OnRenamed;
+            watcher.Changed += OnChanged;
+
+            watcher.Path = Path;
+            watcher.IncludeSubdirectories = IncludeSubdirectories;
+
+            watcher.EnableRaisingEvents = IsEnabled;
+        }
+
+/*        [OnDeserialized]
+        public void PostInit(StreamingContext ctx)
+        {
+            watcher.Created += OnCreated;
+            watcher.Deleted += OnDeleted;
+            watcher.Renamed += OnRenamed;
+            watcher.Changed += OnChanged;
+
+            watcher.Path = Path;
+            watcher.IncludeSubdirectories = IncludeSubdirectories;
+
+            watcher.EnableRaisingEvents = IsEnabled;
+        }*/
+
+        private void OnCreated(object source, FileSystemEventArgs e)
+        {
+            if (!this.ObserveCreation)
+                return;
+
+            App.Current.Dispatcher.Invoke(delegate
+            {
+                EventLogs.Add(new LogEntry($"Файл создан: {e.FullPath}"));
+            });
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            if (!this.ObserveChanges)
+                return;
+
+            App.Current.Dispatcher.Invoke(delegate
+            {
+                EventLogs.Add(new LogEntry($"Файл изменён: {e.FullPath}"));
+            });
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            if (!this.ObserveRename) 
+                return;
+
+            App.Current.Dispatcher.Invoke(delegate
+            {
+                EventLogs.Add(new LogEntry($"Файл переименован: {e.OldFullPath} -> {e.FullPath}"));
+            });
+        }
+
+        private void OnDeleted(object source, FileSystemEventArgs e)
+        {
+            if (!this.ObserveDeletion)
+                return;
+
+            App.Current.Dispatcher.Invoke(delegate
+            {
+                EventLogs.Add(new LogEntry($"Файл удалён: {e.FullPath}"));
+            });
+        }
+
+        public void StopWatching()
+        {
+            watcher.EnableRaisingEvents = false;
+            watcher.Dispose();
+        }
+
+        public override string ToString()
+        {
+            return Path;
+        }
+    }
+
+    public class LogEntry
+    {
+        public DateTime Timestamp { get; }
+        public string Message { get; } = string.Empty;
+
+        public LogEntry(string message)
+        {
+            Timestamp = DateTime.Now;
+            Message = message;
+        }
+
+        public override string ToString()
+        {
+            return $"{Timestamp}: {Message}";
+        }
+    }
+}
